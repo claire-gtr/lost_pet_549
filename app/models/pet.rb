42d64f5395f4d133dc3f7d350207097f30d1c3c1
claire gautier <claire.gautier@edhec.com>
2021-02-16 17:18:32 +0100

    tested pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,6 @@
 class Pet < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  SPECIES = ["dog", "cat", "bunny", "snake", "fish"]
+  validates :species, inclusion: { in: SPECIES}
 end
